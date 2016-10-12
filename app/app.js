b0ad14461e01307634a1c3939e9513a2f6d44ecf
Federico Bond <federicobond@gmail.com>
2016-10-11 22:40:00 -0300

    Add proof of concept ipc for selecting files

--- app/app.js
+++ app/app.js
@@ -3,25 +3,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const { ipcRenderer } = require('electron');
+const ipc = require('electron').ipcRenderer;
 
 // Search component created as a class
 class Search extends React.Component {
 
-    // render method is most important
-    // render method returns JSX template
-    render() {
-        return (
-          <form>
-            <div>Hello World!</div>
-            <input type = "text" />
-            <input type = "submit" />
-          </form>
-        );
-    }
+  constructor() {
+    super();
+    this.state = { path: null };
+  }
+
+  handleOpen() {
+    ipc.once('selected-directory', (event, path) => {
+      this.setState({ path });
+    });
+
+    ipc.send('open-file-dialog');
+  }
+
+  render() {
+    return (
+      <form>
+        <div>Hello World!</div>
+        <button type="button" onClick={this.handleOpen.bind(this)}>Open file</button>
+        {this.state.path && 'You selected: ' + this.state.path}
+      </form>
+    );
+  }
 }
 
 // Render to ID content in the DOM
-ReactDOM.render( < Search / > ,
-    document.getElementById('content')
+ReactDOM.render(<Search />,
+  document.getElementById('content')
 );
