821caef3fe7718dc0559c0deb17831a5ad55c2b2
Federico Bond <federicobond@gmail.com>
2016-10-15 14:00:13 -0300

    Handle unexpected errors

--- app/listeners.js
+++ app/listeners.js
@@ -3,9 +3,14 @@ import {
 }  from './actions'
 import store from './store'
 
+const { dialog } = require('electron').remote
 const ipc = require('electron').ipcRenderer
 
 
 ipc.on('loaded-data', (event, data) => {
   store.dispatch(dataLoaded(data))
 })
+
+window.onerror = function(errorMsg) {
+  dialog.showErrorBox('Unexpected Error', errorMsg)
+}
