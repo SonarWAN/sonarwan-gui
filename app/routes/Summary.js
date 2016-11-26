f295764a15c2ab59ef6b585bbba374231f5b11be
Federico Bond <federicobond@gmail.com>
2016-11-25 21:51:46 -0300

    Add summary data

--- app/routes/Summary.js
+++ app/routes/Summary.js
@@ -4,6 +4,26 @@ import { connect } from 'react-redux'
 
 import Sidebar from '../components/Sidebar'
 
+
+class Statcard extends React.Component {
+  static propTypes = {
+    label: React.PropTypes.string.isRequired,
+    value: React.PropTypes.number.isRequired
+  }
+
+  render() {
+    const { label, value } = this.props
+
+    return (
+      <div className="statcard">
+        <h3>{value}</h3>
+        <p className="lead">{label}</p>
+      </div>
+    )
+  }
+}
+
+
 class Summary extends React.Component {
   static contextTypes = {
     router: React.PropTypes.object
@@ -20,17 +40,39 @@ class Summary extends React.Component {
   }
 
   render() {
+    const { summary } = this.props.data
+
     return (
       <div className="app-body">
         <Sidebar />
         <div className="app-content">
           <h1>Summary</h1>
+
+          <div className="row">
+            <div className="col-md-6">
+              <Statcard label="Devices" value={summary.devices} />
+            </div>
+            <div className="col-md-6">
+              <Statcard label="Authorless services" value={summary.authorless_services} />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              <Statcard label="Files processed" value={summary.files} />
+            </div>
+            <div className="col-md-6">
+              <Statcard label="Packets processed" value={summary.packets} />
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({ data: state.data })
+const mapStateToProps = (state) => ({
+  data: state.data
+})
 
 export default connect(mapStateToProps)(Summary)
